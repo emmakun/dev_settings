75a1be675c82492b5f0ec18cb237cf2546c912aa
Emmanuel Valle <emmanuel.valle@akarisoft.com>
2014-07-27 03:46:22 -0500

    ConsoleZ (Bash): New settings
    Added settings for ConsoleZ app and Bash shell.

---
+++ Windows/ConsoleZ (Bash)/devkit_home/gitshell-config.sh
@@ -0,0 +1,11 @@
+INSERTPATH=":/bin:/c/Users/emmakun/AppData/Local/GitHub/PortableGit_054f2e797ebafd44a30203088cd3d58663c627ef/cmd:/c/Users/emmakun/AppData/Local/GitHub/PortableGit_054f2e797ebafd44a30203088cd3d58663c627ef/bin:/c/Users/emmakun/AppData/Local/Apps/2.0/0C607EYV.CXZ/KXNXT7A1.Y31/gith..tion_317444273a93ac29_0002.0002_f44dcb2e9d4cde94:/c/Windows/Microsoft.NET/Framework/v4.0.30319:/c/Users/emmakun/AppData/Local/Apps/2.0/0C607EYV.CXZ/KXNXT7A1.Y31/gith..tion_317444273a93ac29_0002.0002_f44dcb2e9d4cde94/NativeBinaries/x86:"
+
+export PATH="${PATH/:\/bin:/$INSERTPATH}"
+export GITHUB_GIT="C:\\Users\\emmakun\\AppData\\Local\\GitHub\\PortableGit_054f2e797ebafd44a30203088cd3d58663c627ef"
+export GITHUB_POSH_GIT="C:\\Users\\emmakun\\AppData\\Local\\GitHub\\PoshGit_8aecd991d8ccf3dc78b8cd397ee4e1595f8556d4"
+export GITHUB_SHELL="true"
+export GIT_INSTALL_ROOT="C:\\Users\\emmakun\\AppData\\Local\\GitHub\\PortableGit_054f2e797ebafd44a30203088cd3d58663c627ef"
+export PLINK_PROTOCOL="ssh"
+export POSH_GIT="C:\\Users\\emmakun\\AppData\\Local\\GitHub\\PoshGit_8aecd991d8ccf3dc78b8cd397ee4e1595f8556d4\\profile.example.ps1"
+export SSH_AGENT_PID="180"
+export SSH_AUTH_SOCK="/tmp/ssh-IdvYmkf860/agent.860"
---
+++ Windows/ConsoleZ (Bash)/readme.md
@@ -0,0 +1,22 @@
+## ConsoleZ configuration files
+
+These files are the settings for the ConsoleZ app.
+
+Menlo-Regular font
+: Preferred font, should be installed since is not by default in Windows environments.
+
+console.xml
+: Stores the local configuration selections of the ConsoleZ app.
+
+#### Bash & Git configuration files
+
+The 3 files inside the _devkit_home_ folder should be copied into the current user's _home_ folder and renamed in order to start with a dot (i.e. _bash_profile => _.bash_profile_).
+
+bash_profile
+: Contains the settings for the aliases, prompt and comodities for the Bash shell. Also imports the external sources.
+
+git-prompt.sh
+: Contains the script for showing the Git branch when terminal is inside a repository.
+
+gitshel-config.sh
+: Contains the exports needed to get Git (and git-prompt.sh) working. **This file should be updated with the actual paths obtained after open a repository in "Git Shell" from the GitHub for Windows app. A diff between the environment variables of both shells (Git Shell and target shell) is recommended to get the values to update.**
